2ed050cdb0a0372da3eac905ea076c8c10e136ce
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-07 09:42:14 +0200

    Create iris_train.py

---
+++ iris_train.py
@@ -0,0 +1,14 @@
+import pickle
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+# Load the iris dataset
+iris = load_iris()
+
+# Train a random forest classifier
+rf = RandomForestClassifier()
+rf.fit(iris.data, iris.target)
+
+# Save the model as an artifact
+with open('model.pkl', 'wb') as f:
+    pickle.dump(rf, f)
