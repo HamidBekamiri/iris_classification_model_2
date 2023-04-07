4f2567b9b55a56dfd64f8761667799b4016f7cba
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-04-07 12:00:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 import pickle
-import streamlit as st
+# import streamlit as st
 from sklearn.datasets import load_iris
 
-# Load the iris dataset
-iris = load_iris()
+# # Load the iris dataset
+# iris = load_iris()
+
+# # Load the saved model
+# with open('model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# # Define the prediction function
+# def predict_species(sepal_length, sepal_width, petal_length, petal_width):
+#     features = [[sepal_length, sepal_width, petal_length, petal_width]]
+#     species = model.predict(features)
+#     return iris.target_names[species[0]]
+
+# # Create the Streamlit app
+# st.title("Iris Species Prediction")
+
+# sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.0)
+# sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.0)
+# petal_length = st.slider("Petal Length", 1.0, 7.0, 4.0)
+# petal_width = st.slider("Petal Width", 0.1, 2.5, 1.0)
+
+# if st.button("Predict"):
+#     species = predict_species(sepal_length, sepal_width, petal_length, petal_width)
+#     st.write(f"The predicted species is {species}.")
+from flask import Flask, request, jsonify
+import numpy as np
+
+app = Flask(__name__)
 
 # Load the saved model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Define the prediction function
-def predict_species(sepal_length, sepal_width, petal_length, petal_width):
-    features = [[sepal_length, sepal_width, petal_length, petal_width]]
-    species = model.predict(features)
-    return iris.target_names[species[0]]
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    input_features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(input_features)
+    return jsonify(prediction.tolist())
 
-# Create the Streamlit app
-st.title("Iris Species Prediction")
-
-sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.0)
-sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.0)
-petal_length = st.slider("Petal Length", 1.0, 7.0, 4.0)
-petal_width = st.slider("Petal Width", 0.1, 2.5, 1.0)
-
-if st.button("Predict"):
-    species = predict_species(sepal_length, sepal_width, petal_length, petal_width)
-    st.write(f"The predicted species is {species}.")
+if __name__ == '__main__':
+    app.run()
